005b82a4a70dc49d77a16f82a9079d0de5249ead
Mikita <dixtry3826@gmail.com>
2025-02-06 16:58:18 +0100

    index.css deleted, modified App, added css, images and props validation

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,72 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import './App.css';
+import PropTypes from 'prop-types';
 
-function App() {
-  const [count, setCount] = useState(0)
+class ShopItem {
+  constructor(brand, title, description, descriptionFull, price, currency) {
+    this.brand = brand;
+    this.title = title;
+    this.description = description;
+    this.descriptionFull = descriptionFull;
+    this.price = price;
+    this.currency = currency;
+  }
+}
+
+function ShopItemFunc({ item }) {
+
+  const formattedPrice = `${ item.currency }${ item.price.toFixed(2) }`;
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="main-content">
+      <h2>{ item.brand }</h2>
+      <h1>{ item.title }</h1>
+      <h3>{ item.description }</h3>
+      <div className="description">
+        { item.descriptionFull }
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+      <div className="highlight-window mobile"><div className="highlight-overlay"></div></div>
+      <div className="divider"></div>
+      <div className="purchase-info">
+        <div className="price">{ formattedPrice }</div>
+        <button>Добавить в корзину</button>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    </div>
+  )
+}
+
+// Добавление валидации пропсов
+ShopItemFunc.propTypes = {
+  item: PropTypes.shape({
+    brand: PropTypes.string.isRequired,
+    title: PropTypes.string.isRequired,
+    description: PropTypes.string.isRequired,
+    descriptionFull: PropTypes.string.isRequired,
+    price: PropTypes.number.isRequired,
+    currency: PropTypes.string.isRequired,
+  }).isRequired,
+};
+
+const item = new ShopItem(
+  'Tiger of Sweden',
+  'Leonard coat',
+  'Minimalistic coat in cotton-blend',
+  "Men's minimalistic overcoat in cotton-blend. Features a stand-up collar, concealed front closure and single back vent. Slim fit with clean, straight shape. Above-knee length.",
+  399,
+  '£'
+)
+
+function App() {
+  return (
+    <div className="container">
+      <div className="background-element">
+      </div>
+      <div className="highlight-window">
+        <div className='highlight-overlay'></div>
+      </div>
+      <div className="window">
+        <ShopItemFunc item={ item } />
+      </div>
+    </div>
   )
 }
 
