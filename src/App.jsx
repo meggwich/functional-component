fc4ca75054c2036e7d86167938af860dbe969d3e
Mikita <dixtry3826@gmail.com>
2025-02-10 22:25:57 +0100

    task corrected

--- src/App.jsx
+++ src/App.jsx
@@ -1,50 +1,6 @@
 import './App.css';
-import PropTypes from 'prop-types';
-
-class ShopItem {
-  constructor(brand, title, description, descriptionFull, price, currency) {
-    this.brand = brand;
-    this.title = title;
-    this.description = description;
-    this.descriptionFull = descriptionFull;
-    this.price = price;
-    this.currency = currency;
-  }
-}
-
-function ShopItemFunc({ item }) {
-
-  const formattedPrice = `${ item.currency }${ item.price.toFixed(2) }`;
-
-  return (
-    <div className="main-content">
-      <h2>{ item.brand }</h2>
-      <h1>{ item.title }</h1>
-      <h3>{ item.description }</h3>
-      <div className="description">
-        { item.descriptionFull }
-      </div>
-      <div className="highlight-window mobile"><div className="highlight-overlay"></div></div>
-      <div className="divider"></div>
-      <div className="purchase-info">
-        <div className="price">{ formattedPrice }</div>
-        <button>Добавить в корзину</button>
-      </div>
-    </div>
-  )
-}
-
-// Добавление валидации пропсов
-ShopItemFunc.propTypes = {
-  item: PropTypes.shape({
-    brand: PropTypes.string.isRequired,
-    title: PropTypes.string.isRequired,
-    description: PropTypes.string.isRequired,
-    descriptionFull: PropTypes.string.isRequired,
-    price: PropTypes.number.isRequired,
-    currency: PropTypes.string.isRequired,
-  }).isRequired,
-};
+import ShopItem from './components/ShopItem';
+import ShopItemFunc from './components/ShopItemFunc';
 
 const item = new ShopItem(
   'Tiger of Sweden',
